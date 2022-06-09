38a9fc65ac4700af1f041e3877e2072c4a775c67
Thyago Monnerat <98675898+ThyagoMonnerat@users.noreply.github.com>
2022-06-09 20:28:31 -0300

    Bug fix
    t

--- script.js
+++ script.js
@@ -1,24 +1,54 @@
-let buttonValue = document.getElementById('viewPasswordButton')
-
-let passwordInputType = document.getElementById('passwordInput')
+let passwordInputType = document.getElementById('passwordInputFunction')
 
 let value = 0
 
-function viewPassword(){
-
-  if(value <= 0){
+function viewPassword() {
+  if (value <= 0) {
     viewPasswordButton.classList.add('viewPasswordActive')
 
     passwordInputType.type = 'text'
-    value ++
-  }
-
-  else if(value > 0){
+    value++
+  } else if (value > 0) {
     viewPasswordButton.classList.remove('viewPasswordActive')
     passwordInputType.type = 'password'
     value = 0
   }
-  
-  console.log(value)
-  
+}
+
+let textValue = 0
+let textInput = document.getElementById('inputEmailContainer')
+
+function textInputActive() {
+  if (textValue <= 0) {
+    textInput.classList.add('activeJs')
+    textValue++
+
+    passwordInputGet.classList.remove('activeJs')
+    passwordValue = 0
+  } else if (textValue > 0) {
+    textInput.classList.remove('activeJs')
+    textValue = 0
+
+    passwordInputGet.classList.remove('activeJs')
+    passwordValue = 0
+  }
+}
+
+let passwordValue = 0
+let passwordInputGet = document.getElementById('passwordInput')
+
+function passwordInputActive() {
+  if (passwordValue <= 0) {
+    passwordInputGet.classList.add('activeJs')
+    passwordValue++
+
+    textInput.classList.remove('activeJs')
+    textValue = 0
+  } else if (passwordValue > 0) {
+    passwordInputGet.classList.remove('activeJs')
+    passwordValue = 0
+
+    textInput.classList.remove('activeJs')
+    textValue = 0
+  }
 }
